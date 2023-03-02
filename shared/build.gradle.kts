180b3964258632d73fe2bb961d80b75552f1ce46
Arnav Gupta <championswimmer@gmail.com>
2023-03-02 16:50:12 +0530

    setup: add all common libraries that will needed
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,14 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    kotlin("plugin.serialization")
+    id("app.cash.sqldelight")
 }
+
+val coroutineVersion = "1.6.4"
+val ktorVersion =  "2.2.3"
+val sqlDelightVersion =  "2.0.0-alpha05"
+val dateTimeVersion =  "0.4.0"
 
 kotlin {
     android {
@@ -23,13 +30,26 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutineVersion}")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation("app.cash.sqldelight:runtime:$sqlDelightVersion")
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
             }
         }
-        val androidMain by getting
+        val androidMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
+                implementation("app.cash.sqldelight:android-driver:$sqlDelightVersion")
+            }
+        }
         val androidUnitTest by getting
         val iosX64Main by getting
         val iosArm64Main by getting
@@ -39,6 +59,11 @@ kotlin {
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+                implementation("app.cash.sqldelight:native-driver:$sqlDelightVersion")
+            }
         }
         val iosX64Test by getting
         val iosArm64Test by getting
